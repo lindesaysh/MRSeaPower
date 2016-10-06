65178553326349387e85f3741be637f2b8128c70
lindesaysh <lass@st-and.ac.uk>
2016-10-06 16:34:26 +0100

    update to plotdiffs and powerplot
    power plot to include crosshairs at 80% power

--- R/plot.diffs.R
+++ R/plot.diffs.R
@@ -6,6 +6,7 @@ if(length(grep('raster', search()))==1){
   detach(grep('raster', search()), character.only=TRUE)
 }
 
+library(dplyr)
 
 a<-select(data, x.pos, y.pos, mean)%>%
   mutate(type='mean')
@@ -32,14 +33,14 @@ ggplot(plotdata) + geom_raster(aes( x.pos , y.pos , fill = mean ) ) + scale_fill
 
 
 plot.preds<-function(power.object, returndata=FALSE){
-  
+
   data<-power.object$bootpreds
-  
+
   if(length(grep('raster', search()))==1){
     detach(grep('raster', search()), character.only=TRUE)
   }
-  
-  
+  library(dplyr)
+
   a<-select(data, x.pos, y.pos, eventphase, mean)%>%
     mutate(type='mean')
   b<-select(data, x.pos, y.pos, eventphase, X2.5.)%>%
@@ -48,17 +49,17 @@ plot.preds<-function(power.object, returndata=FALSE){
   c<-select(data, x.pos, y.pos, eventphase, X97.5.)%>%
     mutate(type='Upper_97.5')
   c<-rename(c, mean=X97.5.)
-  
+
   plotdata<-data.frame(rbind(a, b, c))
-  
+
   if(returndata==TRUE){
     return(plotdata)
   }
-  
+
   require(RColorBrewer)
   breaks<-ticks<-c(0, 0.2, 0.4, 0.6, 0.8, 1, 10, 25)
   mypalette<-rev(brewer.pal(length (ticks)-1,"Spectral"))
-  
+
   ggplot(plotdata) + geom_raster(aes( x.pos , y.pos , fill = mean ) ) + scale_fill_gradientn(colours=mypalette, values = breaks, space = "Lab", na.value = "grey50", guide = "colourbar", name='Mean Prediction') + theme_bw() + facet_wrap(~type+eventphase, nrow=3) + coord_equal()
-  
+
 }
--- R/powerPlot.R
+++ R/powerPlot.R
@@ -1,16 +1,24 @@
 powerPlot<-function(power.object){
-  
+
   require(ggplot2)
-  
+
   error.rates<-seq(0.001, 1, by=0.005)
   model.power<-vector(length=length(error.rates))
   nsim.m<-length(power.object$imppvals)
   for(i in 1:length(error.rates)){
     model.power[i]<-(length(which(power.object$imppvals<=error.rates[i]))/nsim.m)*100
   }
-  
+
   dat<-data.frame(err=error.rates, pow=model.power)
-  p <-ggplot(dat) + geom_line(aes(err, pow), color='blue', size=1) + xlab('Error Rate') + ylab('Power') + theme_bw() + scale_y_continuous(limits=c(0,100)) + geom_vline(xintercept=0.05, linetype=2)+ geom_vline(xintercept=0.01, linetype=2)
+
+  id80<-which(dat$pow>79.5)[1]
+  if(length(id80)>=1){
+    dat80<-as.vector(dat[id80[1],])
+  }else{
+    dat80<-as.vector(dat[1,])
+  }
+
+  p <-ggplot(dat) + geom_line(aes(err, pow), color='blue', size=1) + xlab('Error Rate') + ylab('Power') + theme_bw() + scale_y_continuous(limits=c(0,100)) + geom_vline(xintercept=0.05, linetype=2)+ geom_vline(xintercept=0.01, linetype=2) + geom_hline(yintercept=dat80[1,2], linetype=4) + geom_vline(xintercept=dat80[1,1], linetype=4)
   return(p)
 }
 
